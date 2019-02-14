6f79ac0103fa60ad5521bbf91093ae284b51ab1e
Igor Iushchenko [EPAM] <igor_iushchenko@epam.com>
2019-02-14 14:58:15 +0300

    Homework 01 initial.

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import config from '../config/config.json';
+
+import {User, Product} from "./models";
+
+console.log(`Application name: ${config.appName}`);
+const user = new User();
+const product = new Product();
