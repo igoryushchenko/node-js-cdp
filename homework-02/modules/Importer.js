498c5af6776706be0bdaa5904b54208034884c49
igoryushchenko <Igor_Iushchenko@epam.com>
2019-02-26 19:36:05 +0300

    Fix assync import.

--- homework-02/modules/Importer.js
+++ homework-02/modules/Importer.js
@@ -4,30 +4,35 @@ import csvjson from 'csvjson'
 
 class Importer {
   import (path) {
-    let result = []
-    return new Promise((resolve) => {
-      resolve(
-        fs.readdir(path, function (err, files) {
-          if (err) {
-            console.log(err)
-            return
-          }
+    // let result = []
+    let promise = new Promise((resolve, reject) => {
+      fs.readdir(path, function (err, files) {
+        if (err) {
+          console.log(err)
+          return
+        }
 
-          files.filter((each) => each.endsWith('.csv')).forEach((csvFile) => {
-            let data = fs.readFileSync(syspath.join(path, csvFile), { encoding: 'utf8' })
-            result.push(csvjson.toObject(data))
-          })
+        files.filter((each) => each.endsWith('.csv'))
+        resolve(files)
+      })
+    })
+
+    return promise.then(csvFiles => {
+      return new Promise((resolve, reject) => {
+        let jsonData = []
+        csvFiles.forEach(csvFile => {
+          let data = fs.readFileSync(syspath.join(path, csvFile), { encoding: 'utf8' })
+          jsonData.push(csvjson.toObject(data))
         })
-      )
-    }).then(() => {
-      return result
+        resolve(jsonData)
+      })
     })
   }
 
   importSync (path) {
     let result = []
     fs.readdirSync(path).filter((each) => each.endsWith('.csv')).forEach((csvFile) => {
-      let data = fs.readFileSync(syspath.join(path, csvFile), {encoding: 'utf8'})
+      let data = fs.readFileSync(syspath.join(path, csvFile), { encoding: 'utf8' })
       result.push(csvjson.toObject(data))
     })
     return result
