d6e3e4de1791c54cd3fd8f4700121210dd118e5e
igoryushchenko <Igor_Iushchenko@epam.com>
2019-02-26 16:52:35 +0300

    PR fix. Used readdir async.

--- homework-02/modules/Importer.js
+++ homework-02/modules/Importer.js
@@ -4,30 +4,30 @@ import csvjson from 'csvjson'
 
 class Importer {
   import (path) {
+    let result = []
     return new Promise((resolve) => {
       resolve(
-          fs.readdir(path, function (err, files) {
-              if (err) {
-                  console.log(err)
-                  return
-              }
+        fs.readdir(path, function (err, files) {
+          if (err) {
+            console.log(err)
+            return
+          }
 
-              let result = []
-
-              files.filter((each) => each.endsWith('.csv')).forEach((csvFile) => {
-                  let data = fs.readFileSync(syspath.join(path, csvFile), { encoding: 'utf8' })
-                  result.push(csvjson.toObject(data))
-              })
-              return result
+          files.filter((each) => each.endsWith('.csv')).forEach((csvFile) => {
+            let data = fs.readFileSync(syspath.join(path, csvFile), { encoding: 'utf8' })
+            result.push(csvjson.toObject(data))
           })
+        })
       )
+    }).then(() => {
+      return result
     })
   }
 
   importSync (path) {
     let result = []
     fs.readdirSync(path).filter((each) => each.endsWith('.csv')).forEach((csvFile) => {
-      let data = fs.readFileSync(syspath.join(path, csvFile), { encoding: 'utf8' })
+      let data = fs.readFileSync(syspath.join(path, csvFile), {encoding: 'utf8'})
       result.push(csvjson.toObject(data))
     })
     return result
