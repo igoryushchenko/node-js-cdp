d5bbc1dbcb02977472d61d5bceecb1e99cbc331b
Igor Iushchenko [EPAM] <igor_iushchenko@epam.com>
2019-03-01 13:17:52 +0300

    homework number 3

--- homework-03/utils/streams.js
+++ homework-03/utils/streams.js
@@ -1,6 +1,8 @@
-// import commander from 'commander'
 const commander = require('commander')
-​const through​ =​ require('through2'​)
+const trough = require('through2')
+const fs = require('fs')
+const syspath = require('path')
+const csvjson = require('csvjson')
 
 function reverse (str) {
   process.stdin.setEncoding('utf8')
@@ -18,23 +20,71 @@ function reverse (str) {
 }
 
 function transform (str) {
-  console.log('transform')
+  const transformStream = trough(write, end);
+  process.stdin.pipe(transformStream).pipe(process.stdout);
+}
+
+function write (buffer, encoding, next) {
+  this.push(buffer.toString().toUpperCase());
+  next();
+}
+
+function end (done) {
+  done();
 }
 
 function outputFile (filePath) {
-  console.log('outputFile')
+  if (fs.existsSync(filePath)) {
+    const stream = fs.createReadStream(filePath)
+    stream.pipe(process.stdout);
+  } else {
+    printError()
+  }
 }
 
 function convertFromFile (filePath) {
-  console.log('convertFromFile')
+  if (fs.existsSync(filePath) && filePath.endsWith('.csv')){
+    const read = fs.createReadStream(filePath)
+    const toObject = csvjson.stream.toObject()
+    const stringify = csvjson.stream.stringify()
+    read.pipe(toObject).pipe(stringify).pipe(process.stdout)
+  } else {
+    printError()
+  }
 }
 
 function convertToFile (filePath) {
-  console.log('convertToFile')
+  if (fs.existsSync(filePath) && filePath.endsWith('.csv')){
+    const read = fs.createReadStream(filePath)
+    const write = fs.createWriteStream(filePath.replace('.csv', '.json'))
+    const toObject = csvjson.stream.toObject()
+    const stringify = csvjson.stream.stringify()
+    read.pipe(toObject).pipe(stringify).pipe(write)
+  } else {
+    printError()
+  }
 }
 
 function cssBundler (path) {
-  console.log('cssBundler')
+  if (fs.existsSync(path)) {
+    fs.readdir(path, function (err, files) {
+      if (err) {
+        console.log('Error reading directory ' + path)
+        console.log(err)
+        return
+      }
+      const write = fs.createWriteStream(syspath.join(path, 'bundle.css'))
+      for (let file of files) {
+        let data = fs.readFileSync(syspath.join(path, file), { encoding: 'utf8' })
+        write.write(data)
+      }
+
+      let finalData = fs.readFileSync('nodejs-homework3.css', { encoding: 'utf8' })
+      write.write(finalData)
+
+      write.end()
+    })
+  }
 }
 
 function printError () {
