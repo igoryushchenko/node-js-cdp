1326ddf60dddd233a7cfedce3bafea949759344c
Igor Iushchenko [EPAM] <igor_iushchenko@epam.com>
2019-03-12 16:04:07 +0300

    homework number 4. PR fix. Added split module.

--- homework-04/http-servers/html-server.js
+++ homework-04/http-servers/html-server.js
@@ -1,18 +1,23 @@
 const fs = require('fs')
+const trough = require('through2')
+const split = require('split')
+
+function write (buffer, encoding, next) {
+  this.push(buffer.toString().replace('{message}', 'Welcome to the http!'))
+  next()
+}
+
+function end (done) {
+  done()
+}
 
 require('http')
 .createServer()
 .on('request', (req, res) => {
-  let body = ''
-  const fileReadStream = fs.createReadStream('index.html')
-  fileReadStream.on('data', chunk => {
-    body += chunk
-  }).on('close', () => {
-    body = body.replace('{message}', 'Welcome to the http!')
-    res.writeHead(200, {
-      'Content-Type': 'text/html'
-    })
-    res.end(body)
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
   })
+  const transformStream = trough(write, end)
+  fs.createReadStream('index.html').pipe(split()).pipe(transformStream).pipe(res)
 })
 .listen(3000)
