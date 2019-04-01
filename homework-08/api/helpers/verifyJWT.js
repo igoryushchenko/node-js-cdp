7fb4079db993be7a03d5d13c7b3e13dd707b5c2e
Igor Iushchenko [EPAM] <igor_iushchenko@epam.com>
2019-04-01 13:06:30 +0300

    homework number 8. Added security example.

--- homework-08/api/helpers/verifyJWT.js
+++ homework-08/api/helpers/verifyJWT.js
@@ -1,23 +1,17 @@
 import jwt from 'jsonwebtoken'
 
-export default (req, res, next) => {
+module.exports.verifyJwt = function (req, callback) {
   let token = req.headers[process.env.tokenHeaderName]
   if (token) {
     jwt.verify(token, process.env.tokenSecret, (err, decoded) => {
       if (err) {
         console.log(err)
-        res.status(403).json({
-          code: 403,
-          message: 'Token Invalid'
-        })
+        return callback(new Error('Token Invalid'))
       } else {
-        next()
+        return callback(null)
       }
     })
   } else {
-    res.status(403).json({
-      code: 403,
-      message: 'Access Forbidden'
-    })
+    return callback(new Error('Access Forbidden'))
   }
 }
--- homework-08/api/services/authService.js
+++ homework-08/api/services/authService.js
@@ -10,7 +10,7 @@ function getUserByCredentials (email, password) {
         if (err) {
           reject(err)
         } else {
-          resolve(user)
+          resolve(user[0])
         }
       })
     })
